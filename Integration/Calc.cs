781f7c33c1cfb107fc4b235bb470237b59420939
Tam1729 <61475913+Tam1729@users.noreply.github.com>
2021-05-26 15:25:52 +0100

    Add files via upload

---
+++ Integration/Calc.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PUtil.Plot;
+using System.Linq; // don't need it, and have no clue what the fuck it is 
+using System.Numerics; // can use maths like trigs 
+using System;// has Func thing, that's what I need to inculude function as a variable
+
+public class Calc : MonoBehaviour
+{
+    public float a; // initial point
+    public float b;//end point 
+    public int n; // number of integrating steps 
+    public int m; // number of ceoffients or number of accuracy 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //has more steps 
+
+        float i= Integrater.TrapArray(a,b,Function,n); //we set variable i to traparray from integrator to output it's results. we can't do this in integrator because it's statics, so we can't print. 
+        print(i);
+        //uses only first and last value
+        i = Integrater.Trap(a, b, Function);
+        print(i);
+
+
+        float c = (a + b )/ 2; //mid point
+        float d = c - a; //distance between mid point and a or b
+
+        float Symmetric(float t) //off setting a function to make it symmetric if it's not symmetric to start with. but works even if it's symmetric 
+        {
+            return Function(t+c);
+        }
+
+        //plotting it in 2d
+        float[] x = Integrater.Linspace(a,b,n); // arrary of x
+
+        float[] y = new float[n]; //empty arary of y 
+
+        for (int j = 0; j < x.Length; j++)
+        {
+            y[j] = Function(x[j]); // array's of y
+
+        }
+        print("First y "+y[0]);
+        PlotUtil.Plot2D(x,y, Color.magenta, 5f,10f);
+        
+        //Coefficient
+
+        float[] an = Integrater.CoefficientA_n(d, Symmetric, m+1,n); //we need one extra an since bn starts at 1
+        for (int k = 0; k <an.Length; k++)
+        {
+            if (Math.Abs(an[k])<0.001) // neglecting too small value 
+            {
+                an[k] = 0;
+            }
+            print("A" +k + " = " + an[k]);
+        }
+
+        float[] bn = Integrater.CoefficientB_n(d, Symmetric, m, n);
+        for (int k = 0; k < bn.Length; k++)
+        {
+            if (Math.Abs(bn[k]) < 0.001)
+            {
+                bn[k] = 0;
+            }
+            print("B" + (k+1) + " = " + bn[k]);
+        }
+
+        
+        Func<float, float> FourierFunction = Integrater.MakeFunction(an, bn, d); // d is the boundary 
+
+        /*
+        float sum = an[0] / 2;
+        print("c = "+c);
+        for (int l = 0; l < bn.Length; l++)
+        {
+            float v = an[l + 1] * (float)Math.Cos((Math.PI * l * 1) / c);
+            float z = bn[l] * (float)Math.Sin((Math.PI * (l + 1) * 1) / c);
+            print("an "+l +" "+v);
+            print("bn "+l+ " "+z);
+           sum += an[l + 1] * (float)Math.Cos((Math.PI * l * 1) / c) + bn[l] * (float)Math.Sin((Math.PI * (l + 1) * 1) / c); //need to l+1 for sin since it starts at l =0, so when we read bn[l] need to add one to it 
+            
+        }
+        */
+
+        x = Integrater.Linspace(-d,d,n);
+
+        //x = Integrater.Linspace(d, -d, n);
+
+        y = new float[n];
+        for (int j = 0; j < x.Length; j++)
+        {
+            y[j] = FourierFunction(x[j]); // for the fou... 
+        }
+        print("second y "+ y[0]);
+
+        PlotUtil.Plot2D(x, y, Color.yellow, 5f, 10f);
+
+        for (int j = 0; j < x.Length; j++)
+        {
+            y[j] = Symmetric(x[j]); // for the fou... 
+        }
+        print("second y " + y[0]);
+
+        PlotUtil.Plot2D(x, y, Color.white, 5f, 10f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    float Function(float x)
+    {
+        return (float)(Math.Exp(x));
+        //return x*x;
+    }
+}
---
+++ Integration/Integrater.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using System.Numerics;
+
+public static class Integrater // static in memory -can't change-stay as it is so no need update and void the usual thing unity has 
+
+{
+    public static float Trap(float a, float b, Func<float, float> f) // <> creates types of object that can work with any type 
+    {
+        //height -- |initial point - end point|
+        //initial point
+        //end point
+
+        float dx = b - a; //the distance
+        float approx = 0.5f * dx * (f(b) + f(a)); //just the simpla case of trapizum rule, extimating using first and last point
+        return approx;
+    }
+
+    public static float[] Linspace(float a, float b, int n) //integrating steps 
+    {
+        float fraction = (b - a) / (n-1); //the distance 
+        float points = a;// initial point
+        float[] linspace = new float[n]; // creating the array 
+        linspace[0] = a;//assinging the first value array
+
+        int i = 1; // setting the first point to iterate
+        while (points < b) //as long as a != b proceed with this while loop 
+        {
+            points = points + fraction; //next step of the integration
+
+            linspace[i] = points; // filling the empty array created above
+            i++; //incrementing the wile loop value by 1
+            if (i == n) //if i has reached the number of integrating steps we want then break, we don't want more points 
+            {
+                break;
+            }
+        }
+        return linspace;
+    }
+    //adding all the y values at every x 
+    public static float Sum(float[] array, Func<float, float> f) //this returns a float, which takes an array and function 
+    {
+        float sum = 0; //initial sum 
+        for (int i = 0; i <array.Length; i++)
+        {
+            sum += f(array[i]);
+        }
+        return sum;
+    }
+
+    public static float TrapArray(float a, float b, Func<float, float> f, int n)
+    {
+        //height -- |initial point - end point|
+        //initial point
+        //end point
+        //f(x1) +f(x2)  
+        float[] output = Linspace(a, b, n);
+        float wholeSum = Sum(output,f);
+
+        float p = wholeSum - (f(b) + f(a));
+
+        float dx = (b - a) / (n - 1);
+        float approx = 0.5f * dx * (f(b) + f(a) + 2f*(wholeSum-(f(b) + f(a))));
+        return approx;
+    }
+
+
+    public static float[] CoefficientA_n(float c, Func<float, float> f,  int m, int n) // m is how many coefficient we want, n is how many in between steps we want or integration steps
+    {
+
+        float[] An = new float[m]; //float[] that is a array type 
+        for (int i = 0; i < m; i++)
+        {
+            float Function(float x) //anything like this creates a function, in this case takes a float value and returns a float 
+            {
+            return f(x)*(float)Math.Cos((i*Math.PI*x)/c); // we enter the x values, all the parameters is entered by someone so no need to worry about it 
+            } 
+
+             An[i] = (1/c)*TrapArray(-c, c, Function, n); //{} defining the body, () the funcrtion [] array          
+        }
+        return An;                
+     }
+
+    public static float[] CoefficientB_n(float c, Func<float, float> f, int m, int n) // m is how many coefficient we want, n is how many in between steps we want or integration steps
+    {
+        float[] Bn = new float[m]; //float[] that is a array type 
+        for (int i = 1; i < m+1; i++) //sin(0) = 0 so no need to start at 0, and to have the same lenght as an we need m+1
+        {
+            float Function(float x)
+            {
+                return f(x) * (float)Math.Sin((i * Math.PI * x) / c); //(float) function sin, beacuse by default it returns a double
+            }
+
+            Bn[i-1] = (1 / c) * TrapArray(-c, c, Function, n); //{} defining the body, () the funcrtion [] array
+        }
+        return Bn;
+    }
+    //this outputs a function which takes an arrays of an,ba, and the boundary
+    public static Func<float,float> MakeFunction(float[] An, float[] Bn, float boundary)
+    {        
+        if (boundary < 0) //taking the positive value for the boundary 
+        { 
+            boundary = -boundary; 
+        }
+
+        float g(float t) //creating a function that outputs a float and takes in a float 
+        {
+            float sum = An[0]/2; //seeting the initial value for the sum 
+
+            for (int l = 0; l <Bn.Length; l++) //going upto bn's length since bn has one more vlue then an in the summation, as an losses a0 
+            {
+                
+                sum += An[l + 1] * (float)Math.Cos((Math.PI * (l+1) * t) / boundary) + Bn[l] * (float)Math.Sin((Math.PI * (l+1) * t) / boundary); //need to l+1 for sin since it starts at l =0, so when we read bn[l] need to add one to it 
+            }
+
+            return sum;
+        }
+        
+        return g; // fourier series f(x)
+    }
+
+}
